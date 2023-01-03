822f07369f1f40469b7105bc0e472e02eaacc6e9
Diana Maria Stefan <94044563+diana389@users.noreply.github.com>
2023-01-04 01:36:47 +0200

    submitted

---
+++ Assignment 1/Main.java
@@ -0,0 +1,21 @@
+package Assignment1;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Album("Album1", 100, "excellent");
+        Book book2 = new Novel("Ion", 350, "psychological");
+        Book book3 = new Album("Album2", 75, "good");
+
+        Catalog catalog = new Catalog();
+
+        catalog.add(book1);
+        catalog.add(book2);
+        catalog.add(book3);
+
+        catalog.list();
+
+        catalog.delete(book2);
+
+        catalog.list();
+    }
+}
