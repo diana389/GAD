26018b6958e301b1d8a6d73e68e500ae54b8eb22
Diana Maria Stefan <94044563+diana389@users.noreply.github.com>
2023-01-04 20:17:14 +0200

    submitted

---
+++ Assignment 2/Contact.java
@@ -0,0 +1,22 @@
+package Assignment2;
+
+public class Contact {
+    String phoneNumber;
+    String firstName;
+    String lastName;
+
+    public Contact(String phoneNumber, String firstName, String lastName) {
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
