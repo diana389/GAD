2927a87b89919fb325c58c1b2d2084fee73843c1
Diana Maria Stefan <94044563+diana389@users.noreply.github.com>
2023-01-04 21:33:50 +0200

    submitted

--- Assignment 3/Map.java
+++ Assignment 3/Map.java
@@ -5,15 +5,12 @@ import java.util.HashMap;
 
 public class Map {
     HashMap<Persoana, ArrayList<Hobby>> pairs = new HashMap<Persoana, ArrayList<Hobby>>();
-
     public void addPair(Persoana persoana, ArrayList<Hobby> hobbies) {
         pairs.put(persoana, hobbies);
     }
 
     @Override
     public String toString() {
-        return "Map{" +
-                "pairs=" + pairs +
-                '}';
+        return "Map{" + "pairs=" + pairs + '}';
     }
 }
